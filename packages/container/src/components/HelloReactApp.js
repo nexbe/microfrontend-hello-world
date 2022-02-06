44e9ffc5fb91039caa5dd2f79d9fa58610363f2e
Ken Ling <ken88ling@gmail.com>
2022-02-06 17:36:01 +0800

    init

--- packages/container/src/components/HelloReactApp.js
+++ packages/container/src/components/HelloReactApp.js
@@ -1,15 +1,15 @@
-import { mount } from 'helloReact/HelloReactApp'
-import React, { useRef, useEffect } from 'react'
+import { mount } from 'helloReact/HelloReactApp';
+import React, { useRef, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export default () => {
-    const ref = useRef(null);
-    const history = useHistory();
+  const ref = useRef(null);
+  const history = useHistory();
 
-    useEffect(() => {
-        const { onParentNavigate } = mount(ref.current)
-        history.listen(onParentNavigate)
-    }, [])
+  useEffect(() => {
+    const { onParentNavigate } = mount(ref.current);
+    history.listen(onParentNavigate);
+  }, []);
 
-    return <div ref={ref} />
-}
+  return <div ref={ref} />;
+};
